32466807d8fdbe3a55628a8eaa19e62210eaee78
Jonathan Keane <jkeane889@gmail.com>
2019-04-25 10:41:20 -0400

    Script to remove values from dictionary based on string length

---
+++ mod2_objects4_3.js
@@ -0,0 +1,33 @@
+/*
+
+Write a function called "removeStringValuesLongerThan".
+
+Given an number and an object, "removeStringValuesLongerThan" removes any properties on the given object whose values are strings longer than the given number.
+
+var obj = {
+  name: 'Montana',
+  age: 20,
+  location: 'Texas'
+};
+removeStringValuesLongerThan(6, obj);
+console.log(obj); // { age: 20, location: 'Texas' }
+
+*/
+
+function removeStringValuesLongerThan(num, obj) {
+    for (var key in obj) {
+        var value = obj[key];
+        if (value.length > num) {
+            delete obj[key]
+        }
+    }
+    return obj
+}
+
+var obj = {
+  name: 'Montana',
+  age: 20,
+  location: 'Texas'
+};
+removeStringValuesLongerThan(6, obj);
+console.log(obj); // { age: 20, location: 'Texas' }
