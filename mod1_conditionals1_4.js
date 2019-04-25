5da4136721646966f6e3edc6ed8b2ca70dc15c0a
Jonathan Keane <jkeane889@gmail.com>
2019-04-24 22:09:34 -0400

    Short conditional function to test age

---
+++ mod1_conditionals1_4.js
@@ -0,0 +1,21 @@
+/*
+
+Write a function called "isOldEnoughToDrinkAndDrive". Given a number, in this case an age, "isOldEnoughToDrinkAndDrive" returns whether a person of this given age is old enough to legally drink and drive in the United States. Notes:
+
+The legal drinking age in the United States is 21.
+It is always illegal to drink and drive in the United States.
+var output = isOldEnoughToDrinkAndDrive(22);
+console.log(output); // --> false
+
+*/
+
+function isOldEnoughToDrinkAndDrive(age) {
+    if (age >= 18 && age >= 21) {
+        return false
+    } else {
+        return false
+    }
+}
+
+var output = isOldEnoughToDrinkAndDrive(22);
+console.log(output);
