6b4f9619de9f58d7d55fe3e3cfaa9a2a4a32c91b
Jonathan Keane <jkeane889@gmail.com>
2019-04-25 20:27:14 -0400

    Function to split string based on separator and rejoin

---
+++ mod1_strings2_1.js
@@ -0,0 +1,22 @@
+/*
+
+Write a function called "convertDoubleSpaceToSingle".
+
+Given a string, "convertDoubleSpaceToSingle" returns the passed in string, with all the double spaces converted to single spaces.
+
+var output = convertDoubleSpaceToSingle("string  with  double  spaces");
+console.log(output); // --> "string with double spaces"
+Notes:
+
+In order to do this problem, you should be familiar with "String.split", and "Array.join".
+
+*/
+
+function convertDoubleSpaceToSingle(str) {
+    var newString = str.split("  ");
+    var strArray = newString.join(" ");
+    return strArray
+}
+
+var output = convertDoubleSpaceToSingle("string  with  double  spaces");
+console.log(output); // --> "string with double spaces"
