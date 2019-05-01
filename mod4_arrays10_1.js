53d585579c1b51e83249b568ca232fc5edc20a76
Jonathan Keane <jkeane889@gmail.com>
2019-05-01 08:11:11 -0400

    Funtion to return product total of elements in an array

---
+++ mod4_arrays10_1.js
@@ -0,0 +1,29 @@
+/*
+
+Write a function called "computeProductOfAllElements".
+
+Given an array of numbers, "computeProductOfAllElements" returns the products of all the elements in the given array.
+
+Notes:
+
+If given array is empty, it should return 0.
+
+*/
+
+function computeProductOfAllElements(arr) {
+    if (arr.length != 0) {
+        var product = 1
+        for (var i = 0; i < arr.length; i++) {
+            product *= arr[i];
+        }
+        return product
+    } else {
+        return 0
+    }
+}
+
+var output = computeProductOfAllElements([2, 5, 6]);
+console.log(output); // --> 60
+
+
+    
